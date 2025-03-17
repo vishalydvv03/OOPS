87e19262730016d5821f76622fe3bb6cd0124eca
Vishal Yadav <vishaljyadav576@gmail.com>
2025-03-17 16:19:45 +0530

    Implemented Abstraction

---
+++ Abstraction/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    //abstract class Person
+    //{
+    //    public void Walk()
+    //    {
+    //        Console.WriteLine("I can walk");
+    //    }
+
+    //    public abstract void Greet();
+    //}
+
+    interface IPerson
+    {
+        void Greet();
+    }
+    
+    class Student : IPerson
+    {
+        public string Name;
+
+        public void Greet()
+        {
+            
+            Console.WriteLine("Hello " + Name);
+
+        }
+    }
+
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stud = new Student();
+            stud.Name = "Vishal";
+            stud.Greet();
+        }
+    }
+}
