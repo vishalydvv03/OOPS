764152f67bf74537e5e272f657f6b3751f35b5f2
Vishal Yadav <vishaljyadav576@gmail.com>
2025-03-17 16:05:12 +0530

    Implemented Polymorphism

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Operations 
+    {
+        public virtual int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public virtual int Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+    }
+
+    public class Addition : Operations
+    {
+        public override int Add(int x, int y)
+        {
+            int sum = x + y;
+            return sum;
+        }
+
+        //Method Overloading -> Compile Time Polymorphism
+        public int Add(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Addition op = new Addition();
+
+            Console.WriteLine(op.Add(2,3));
+            Console.WriteLine(op.Add(2,3,4));
+        }
+    }
+}
