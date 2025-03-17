f4ef53565325f7eb73c8097ae602b7c5c6dd8115
Vishal Yadav <vishaljyadav576@gmail.com>
2025-03-17 17:11:20 +0530

    Out vs Ref Parameters

---
+++ Parameters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+    class Program
+    {
+        public static void Swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public static int Change(out int a)
+        {
+            a = 0;
+            return a+30;  //when using out, it needs to be initialized inside the function
+        }
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            // Before Ref
+            //Swap(x, y);
+            //Console.WriteLine("x= " + x + " y= " + y);
+            Swap(ref x, ref y);
+            Console.WriteLine("x= " +x + " y= " +y);
+
+            x= Change(out x);
+            Console.WriteLine(x);
+        }
+    }
+}
